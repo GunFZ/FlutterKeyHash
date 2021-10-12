8c9d08922be314886719af33c5bf4d73756f4265
NguyenTienDat1303 <49563850+ntdvn@users.noreply.github.com>
2021-10-12 14:42:23 +0700

    fix sound null safety

--- lib/key_hash.dart
+++ lib/key_hash.dart
@@ -1,12 +1,10 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:package_info/package_info.dart';
 
 class KeyHash {
-  static const MethodChannel _channel =
-      const MethodChannel('key_hash');
+  static const MethodChannel _channel = const MethodChannel('key_hash');
 
   static Future<String> get getKeyHash async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
@@ -17,7 +15,8 @@ class KeyHash {
     String buildNumber = packageInfo.buildNumber;
     print(packageName);
 
-    final String version1 = await _channel.invokeMethod('getKeyHash', packageName);
+    final String version1 =
+        await _channel.invokeMethod('getKeyHash', packageName);
     return version1;
   }
 }
